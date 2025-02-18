fb1766a665fd451580a2250dc46d26a30dc12bc3
komuntale elizabeth <komuntaleliz152@gmail.com>
2025-02-18 10:37:35 +0300

    javascript

---
+++ dataTypes.js
@@ -0,0 +1,30 @@
+//strings-a set of characters that form a word
+//number-numerical character
+//boolean-a YES or NO
+//discrete data- involves intergers for a limited number of values
+//array- a list
+//symbol
+//integer- a number which is discrete
+//undefined
+//null
+//date
+//function
+//symbol
+//value
+//real
+//dec
+//bigint
+//object
+
+//primitive- we store them by their actual value
+//number-numerical character
+//strings-a set of characters that form a word
+//boolean-a YES or NO e.g isloggedin=True
+//undefined e.g X or Y
+//null e.g Y=null
+//bigint e.g big Number=555555555555555555555555555
+
+
+
+//variable
+//value
---
+++ variables.js
@@ -0,0 +1,24 @@
+//variable
+//value
+
+//primitive- we store them by their actual value
+//number-numerical character e.g 1,2,3,... 
+//strings-a set of characters that form a word e.g 'egg',"egg",'egg'
+//boolean-a YES or NO e.g isloggedin=True
+//undefined e.g X or Y
+//null e.g Y=null
+//bigint e.g big Number=555555555555555555555555555
+
+//declare e.g a,b,x,y,num1,num2
+        //firstName,lastname,age,email,password 
+        //boolean e.g isloggedIn, isMarried isAlive
+        //lett, const, var
+//assign e.g nameofVariable=value
+let firstName="Komuntale";
+let lastname="Liz";
+let age=21;
+let isMarried=false;
+let city="Fort portal";
+let country="Uganda";
+const gravity=131313131
+console.log(firstName,lastname,age,isMarried,city,country,gravity);
